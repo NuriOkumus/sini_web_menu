163be003c14f3e046ca6fa2305d3b83f37d03725
Nuri <eylulvenuri2@gmail.com>
2025-01-25 18:45:33 +0300

    Update base href for GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3535788350"
+    serviceWorkerVersion: "4253700395"
   }
 });
